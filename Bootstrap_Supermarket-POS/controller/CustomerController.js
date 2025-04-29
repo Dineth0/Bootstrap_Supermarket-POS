3e39eb9af426f093e5522ed62648777229b92e09
Dineth0 <dinethnakandala10@gmail.com>
2025-04-29 15:00:07 +0530

    load customer to table

--- Bootstrap_Supermarket-POS/controller/CustomerController.js
+++ Bootstrap_Supermarket-POS/controller/CustomerController.js
@@ -1,13 +1,16 @@
 import {customer_db} from "../db/db.js";
 import CustomerModel from "../model/CustomerModel.js";
 
-$("#customer-save").on("click", function () {
-    let id = $("#id").val();
-    let name = $('#name').val();
+
+
+
+$('#customer-save').on("click", function () {
+    let customerId = $("#id").val();
+    let customerName = $('#name').val();
     let address = $('#address').val();
     let number = $('#number').val();
 
-    if(id === '' || name === '' || address === '' || number === '' || number === '') {
+    if(customerId === '' || customerName === '' || address === '' || number === '') {
         Swal.fire({
             title: 'Error!',
             text: 'Invalid Inputs',
@@ -15,13 +18,52 @@ $("#customer-save").on("click", function () {
             confirmButtonText: 'Ok'
         });
     }else {
-        let customer_data = new CustomerModel(id, name, address, number, number);
+        let customer_data = new CustomerModel(customerId, customerName, address, number);
         customer_db.push(customer_data);
         console.log(customer_db);
+        loadCustomerTableData();
         Swal.fire({
             title: "Added Successfully!",
             icon: "success",
             draggable: true
 
         })
-    }});
+    }
+});
+
+
+
+function loadCustomerTableData(){
+             $('#customer-tbody').empty();
+             customer_db.map((item, index) => {
+                 let customerId = item.customerId;
+                 let customerName = item.customerName;
+                 let address = item.address;
+                 let phoneNumber = item.phoneNumber;
+
+                 let data = `<tr>
+                     <td>${customerId}</td>
+                     <td>${customerName}</td>
+                     <td>${address}</td>
+                     <td>${phoneNumber}</td>
+                 </tr>`
+
+                 $('#customer-tbody').append(data);
+             });
+}
+
+$('#customer-tbody').on('click', 'tr', function () {
+    let id = $(this).index();
+    let obj = customer_db[id];
+
+    let customerId = obj.customerId;
+    let customerName = obj.customerName;
+    let address = obj.address;
+    let phoneNumber = obj.phoneNumber;
+
+    $('#id').val(customerId);
+    $('#customerName').val(customerName);
+    $('#address').val(address);
+    $('#number').val(phoneNumber);
+})
+
